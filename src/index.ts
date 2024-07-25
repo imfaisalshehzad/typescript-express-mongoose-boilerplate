4f7dd4f17ed89bb373ca90ca8604e00cc3cd56d3
faisalshehzad <fshehzad@digitalandcode.com>
2024-07-25 14:54:51 +0400

    docker

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {json} from "body-parser";
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from "./routes/user";
 
 dotenv.config();
 
@@ -9,13 +10,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(json());
 
+app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
-    res.send("Hello World!!!!");
+    res.send("Hello World!!!");
 });
 
 const startServer = async () => {
     try {
-        await mongoose.connect(`${process.env.MONOGO_DB_URI}`);
+        await mongoose.connect(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongo_js:27017/expressjs?authSource=admin`);
         console.log('MongoDB connected..');
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
